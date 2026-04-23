1880a731f9d4f6778c5ec10ecabbb308ccb7fcf3
Nicolo Valle <nicolo.valle@cern.ch>
2026-04-23 15:34:15 +0200

    A working producer for FERS data replay

--- user/hidra/dry/src/HidraDryFERSProducer.cc
+++ user/hidra/dry/src/HidraDryFERSProducer.cc
@@ -4,8 +4,11 @@
 #include <ratio>
 #include <chrono>
 #include <thread>
+#include <limits>
+#include <bit>
+#include <cstdint>
 #include <iterator>     // std::ostream_iterator
-#include <algorithm>    // std::copy
+#include <algorithm>    // std::copy, std::min, std::max
 
 #define FILE_HEADER_SIZE	25
 #define EVENT_HEADER_SIZE	27
@@ -22,6 +25,9 @@ public:
   void DoTerminate() override;
   void DoReset() override;
   void ReadRawData(std::vector<char>& block, uint16_t data_size);
+  std::string GetEventInfo(eudaq::Event* ev);
+  uint64_t getTimeus();
+  void sleepUntilNext(uint64_t b_last_evt, uint64_t b_current_evt, uint64_t last_real); // all in usec
   void ReadFileSize();
   void Mainloop();
   
@@ -30,6 +36,7 @@ private:
   std::ifstream m_ifile;
   uint64_t m_ifile_size;
   std::string m_data_in_path;
+  int m_event_spacing; // microsec
   std::thread m_thd_run;
   mutable bool m_exit_of_run;
 
@@ -53,8 +60,11 @@ void HidraDryFERSProducer::DoInitialise(){
 
 void HidraDryFERSProducer::DoConfigure(){
   auto conf = GetConfiguration();
-  m_data_in_path = conf->Get("DATA_IN_PATH", "infile.txt");
+  m_data_in_path = conf->Get("DATA_IN_PATH", "infile.dat");
   EUDAQ_INFO("Using FERS raw data file " + m_data_in_path);
+  m_event_spacing = 1000* (int) conf->Get("REPLAY_EVENT_SPACING_MS", -1);
+  std::string inforeplay = m_event_spacing < 0 ? "automatic" : std::to_string(m_event_spacing) + " us";
+  EUDAQ_INFO("Replay rate set to "+inforeplay);
   ReadFileSize();
 }
 
@@ -66,7 +76,7 @@ void HidraDryFERSProducer::DoStartRun(){
 
 void HidraDryFERSProducer::DoStopRun(){
   m_exit_of_run = true;
-  EUDAQ_INFO("Exiting DualROCalo Run");
+  EUDAQ_INFO("Exiting HidraDryFERSProducer Run");
   if(m_thd_run.joinable()){
     m_thd_run.join();
   }
@@ -105,149 +115,207 @@ void HidraDryFERSProducer::ReadRawData(std::vector<char>& block, uint16_t data_s
 }
 
 
+std::string HidraDryFERSProducer::GetEventInfo(eudaq::Event* ev){
+  std::string info = "Event Info:";
+  info += " evtN "+std::to_string(ev->GetEventN());
+  info += " trgN "+std::to_string(ev->GetTriggerN());
+  info += " start/stop "+std::to_string(ev->GetTimestampBegin())+"/"+std::to_string(ev->GetTimestampEnd());
+  info += " nblk "+std::to_string(ev->GetNumBlock());
+  return info;
+}
 
+uint64_t HidraDryFERSProducer::getTimeus(){
+  auto now = std::chrono::system_clock::now();
+  auto us = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count();
+  return (uint64_t)us;
+}
+
+void HidraDryFERSProducer::sleepUntilNext(uint64_t last_evt, uint64_t current_evt, uint64_t last_real){
+  if (last_real < 999) {
+    EUDAQ_ERROR("Meaningless timestamp of the last event sent ("+std::to_string(last_real)+")");
+    return;
+  }
+  if (m_event_spacing == 0) return;
+  if (m_event_spacing > 0){
+    uint64_t n = getTimeus();
+    if ((n > last_real) && (n-last_real) < m_event_spacing){
+      std::this_thread::sleep_for(std::chrono::microseconds(m_event_spacing - (n-last_real)));
+      return;
+    }
+  }
+  else{ // m_event_spacing < 0
+    
+    if (current_evt < last_evt){
+      EUDAQ_ERROR("Current event timestamp "+std::to_string(current_evt)+" < last "+std::to_string(last_evt));
+      return;
+    }
+    
+    uint64_t n = getTimeus();
+    
+    //EUDAQ_DEBUG("sleepUtils "+std::to_string(last_evt)+" "+std::to_string(current_evt)+" "+std::to_string(last_real)+" "+std::to_string(n));
+   
+    if ( (n-last_real) < (current_evt - last_evt)){ 
+      int usec = (int)((current_evt - last_evt) - (n-last_real));
+      if (usec > 500000){
+	EUDAQ_DEBUG("Sleeping "+std::to_string(usec/1000)+ " ms");
+      }
+      std::this_thread::sleep_for(std::chrono::microseconds(usec));
+    }
+    
+    return;
+  }
+}
 
 void HidraDryFERSProducer::Mainloop(){  
   //auto tp_start_run = std::chrono::steady_clock::now();
-  uint64_t loop_count = 0;
-  EUDAQ_INFO("Mainloop():: m_exit_of_run = "+std::to_string(m_exit_of_run));
+ 
+  EUDAQ_INFO("Entering Mainloop");
 
   while(!m_exit_of_run){
     
     uint16_t data_size = MAX_EVENT_SIZE;
     
-    // Skip bytes (file header)
+    // file header
     // TODO: send a BORE with this
     uint8_t header_size = FILE_HEADER_SIZE; // 25 bytes	
-    std::vector<char> file_header(header_size);
-    m_ifile.read(file_header.data(), header_size);
+    std::vector<uint8_t> file_header(header_size);
+    m_ifile.read(reinterpret_cast<char*>(file_header.data()), header_size);
+    int file_run_number;
+    memcpy(&file_run_number, &file_header[7], 2);
+    EUDAQ_WARN("Run number from file is "+std::to_string(file_run_number));
+    eudaq::mSleep(2000);
 
-    uint8_t time_unit = (uint8_t) file_header[12]; // this steers the data size from each board
-    int event_board_size = -1;
-
-    // TODO: following is valid only for Spectroscopy + Timing
-    if (time_unit == 0) event_board_size = 12; // bytes
-    else if (time_unit == 1) event_board_size = 14; // bytes
-    else{
-      EUDAQ_ERROR("Incorrect information from file header. Time Unit is "+ std::to_string(time_unit) + ", expected 0 or 1");
-      // TODO: stop run
-    }
+   
 
     int block_id = 0;
-
     std::vector<char> block(data_size);
-    std::vector<char> zero(6,0);
+    std::vector<char> zeros(6,0);
+   
+    uint64_t current_trigger_id = std::numeric_limits<uint64_t>::max();
 
-    while (m_ifile.read(block.data(), block.size())){
-      // if statement needed to stop reading instead of having to wait until end of file is reached
+    //std::unique_ptr<eudaq::Event> current_evt;
+    eudaq::EventUP current_evt;
+    bool have_open_evt = false;
+    uint64_t min_timestamp = std::numeric_limits<uint64_t>::max();
+    uint64_t max_timestamp = 0;
+
+    uint64_t evt_time_last_sent = 0;
+    uint64_t real_time_last_sent = 0;
+
+    uint64_t iblock = 0;
+    uint64_t ievt = 0;
+    for (;; iblock++){
+
       if(m_exit_of_run==true) break;
 
-      
       auto ev = eudaq::Event::MakeUnique("FERSEvent");
       
-      uint16_t event_size;
-      memcpy(&event_size, &block[0], 2);
+      std::vector<uint8_t> eventsize_b(2); // 
 
-
-      if(loop_count==0){
-	EUDAQ_DEBUG("Mainloop():: Block[0] = " + std::to_string((uint8_t)block[0]));
-	EUDAQ_DEBUG("Mainloop():: Block[1] = " + std::to_string((uint8_t)block[1]));
-	EUDAQ_DEBUG("Mainloop():: event_size = " + std::to_string(event_size));
+      if (  !  m_ifile.read(reinterpret_cast<char*>(eventsize_b.data()), eventsize_b.size()) ){
+	m_exit_of_run = true;
+	EUDAQ_INFO("File finished. Event counter: "+std::to_string(iblock));
+	continue; // should be equivalent to break here
       }
 
-      loop_count++;
 
-      std::vector<uint8_t> hits;
-    
-      uint8_t board_id;
-      memcpy(&board_id, &block[2], 1);
-      hits.push_back(board_id);
-			
-      double trigger_time_stamp;
-      memcpy(&trigger_time_stamp, &block[3], 8);
-      ev->SetTimestamp(trigger_time_stamp, trigger_time_stamp+100, true);
-      
-      uint64_t trigger_id;
-      memcpy(&trigger_id, &block[11], 8);
-      ev->SetTriggerN(trigger_id);
+      uint16_t event_size;
+      memcpy(&event_size, &eventsize_b[0], 2);
 
- 
-      uint64_t channel_mask;
-      memcpy(&channel_mask, &block[19], 8);
-      
-      //uint8_t num_channels = sizeof(channel_mask)*8;
-      int num_channels = 0;
-      if ((event_size - EVENT_HEADER_SIZE) % event_board_size == 0){
-	num_channels = (event_size - EVENT_HEADER_SIZE) / event_board_size;
+      if (event_size < 2 || event_size > MAX_EVENT_SIZE){
+	EUDAQ_ERROR("Block "+std::to_string(iblock)+", inconsistent event size "+std::to_string(event_size));
+	m_exit_of_run = true; // cannot decode anymore if the event size is not correct
+	continue; 
       }
       else{
-	EUDAQ_ERROR("event size ("+std::to_string(event_size)+") - header size ("+std::to_string(EVENT_HEADER_SIZE)+") is not multiple of "+std::to_string(event_board_size)+": cannot compute number of baords");
-	// TODO: stop run
+	//EUDAQ_DEBUG("Block "+std::to_string(iblock)+" size: "+std::to_string(event_size)); 
       }
 
       
-      uint8_t channel_mask_bits[num_channels];
-      uint8_t num_active_channels = 0;
-      int channel_pos = 0;
-      for (int b=0; b<num_channels; b++) {
-			
-	channel_mask_bits[b] = (channel_mask >> b) & 1;
+      
+      std::vector<uint8_t> eventblock(event_size - 2);
 
-	if(channel_mask_bits[b] == 1) {
-	  num_active_channels++;
-	  int channel_offset = 27+channel_pos;
-	  hits.insert(hits.end(), block.begin()+channel_offset, block.begin()+channel_offset+6);
-	  uint8_t data_type=block[channel_offset+1];
-	  if (data_type==0x03) {
-	    channel_pos+=6;
-	    hits.insert(hits.end(), zero.begin(), zero.begin()+6);
-	  }
-	  else if (data_type==0x33) {
-	    channel_pos+=12;
-	    hits.insert(hits.end(), block.begin()+channel_offset+6, block.begin()+channel_offset+12);
-	  }
-	  else {
-	    std::ostringstream ss;
-	    ss << "0x" << std::hex << data_type;
-	    // TODO: frequently entering here. why?
-	    EUDAQ_DEBUG("Unsupported Data Type: "+ss.str());
-	    break;
-	  }
-	}
-      }
+      m_ifile.read(reinterpret_cast<char*>(eventblock.data()), eventblock.size());
       
-      if (loop_count==1){
-	EUDAQ_DEBUG("eventsize = " + std::to_string(event_size));
-	EUDAQ_DEBUG("board_id = " + std::to_string(board_id));
-	EUDAQ_DEBUG("trigger_time_stamp = " + std::to_string(trigger_time_stamp));
-	EUDAQ_DEBUG("trigger_id = " + std::to_string(trigger_id));
-	EUDAQ_DEBUG("num_active_channels = " + std::to_string(num_active_channels));
-	//EUDAQ_DEBUG("channel_mask_bits = " + std::to_string(channel_mask));
-	std::cout<< "channel mask bits = " << std::endl;
-	for (int b=0; b<num_channels; b++){
-	  std::cout << std::to_string(channel_mask_bits[b]) << " ";
-	  //if ((b+1)%8 ==0) std::cout << std::endl;
-	}
-				
-      }
       
-	       
-      ev->AddBlock(0, hits);
+
+      uint64_t trigger_id = 0;
+      memcpy(&trigger_id, &eventblock[9], 8);
+
+      double d_trigger_timestamp;
+      memcpy(&d_trigger_timestamp, &eventblock[1], 8);
+      //trigger_timestamp = *static_cast<double*>&eventblock[1]
+      uint64_t trigger_timestamp = static_cast<uint64_t>(d_trigger_timestamp*1000);
+      
+      uint8_t board_id;
+      memcpy(&board_id, &eventblock[0], 1);
+
+    
+      if (!have_open_evt){
+	EUDAQ_INFO("Creating the first event with trigID "+std::to_string(trigger_id));
+
+	current_evt = eudaq::Event::MakeUnique("FERSEvent");
+	current_trigger_id = trigger_id;
+	current_evt->SetTriggerN(current_trigger_id);
+	current_evt->SetEventN(current_trigger_id);
+	current_evt->SetRunN(file_run_number);
+	
+	have_open_evt = true;
+      }
+
+      if (have_open_evt && trigger_id > current_trigger_id){
+
+	current_evt->SetTimestamp(min_timestamp, max_timestamp, true);
+
+	sleepUntilNext(evt_time_last_sent/1000, current_evt->GetTimestampBegin()/1000, real_time_last_sent);
+	
+	EUDAQ_INFO("Sending Dry FERS event "+std::to_string(ievt)+" (at block "+std::to_string(iblock)+")-- "+GetEventInfo(current_evt.get()));
+	evt_time_last_sent = current_evt->GetTimestampBegin();
+	real_time_last_sent = getTimeus();
+	SendEvent(std::move(current_evt));
+	ievt++;
+	
+
+	current_evt = eudaq::Event::MakeUnique("FERSEvent");
+	current_trigger_id = trigger_id;
+	min_timestamp = std::numeric_limits<uint64_t>::max();
+	max_timestamp = 0;
+	current_evt->SetTriggerN(current_trigger_id);
+	current_evt->SetEventN(current_trigger_id);
+	current_evt->SetRunN(file_run_number);
+      }
+
+      
+      if (have_open_evt && trigger_id < current_trigger_id){
+	EUDAQ_ERROR("Old trigger id detected: "+std::to_string(trigger_id)+", most recent is "+std::to_string(current_trigger_id)+". Skipping block "+std::to_string(iblock)+" in evt "+std::to_string(ievt));
+	continue; // skip this block
+      }
+
+      
+      min_timestamp = std::min(min_timestamp, trigger_timestamp);
+      max_timestamp = std::max(max_timestamp, trigger_timestamp);
+      current_evt->AddBlock(board_id, eventblock); // TODO better to use progressive index ?
+
      
-      block_id++;
-      
-      if (loop_count==1){
-	ev->Print(std::cout);
-	//eudaq::mSleep(10000);
-      }
-      SendEvent(std::move(ev));
+    } // end of iblock loop
 
-      
-
+    if (have_open_evt && current_evt){
+      current_evt->SetTimestamp(min_timestamp, max_timestamp, true);
+      sleepUntilNext(evt_time_last_sent/1000, current_evt->GetTimestampBegin()/1000, real_time_last_sent);
+      EUDAQ_INFO("SSending Dry FERS event "+std::to_string(ievt)+" (at block "+std::to_string(iblock)+")-- "+GetEventInfo(current_evt.get()));
+      SendEvent(std::move(current_evt));
+      ievt++;
     }
-    
-    m_exit_of_run=true;
-    
+
+    m_exit_of_run = true;
   }
-  EUDAQ_DEBUG("EXITING MAINLOOP");
+  EUDAQ_INFO("EXITING MAINLOOP THREAD");
 }
+      
+      
+	
+	
+   
+   
+
+
