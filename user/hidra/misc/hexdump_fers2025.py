2d450dff6219b0810a38fdef1f3cbae854622ff3
Nicolo Valle <nicolo.valle@cern.ch>
2026-04-29 10:11:11 +0200

    more verbose dump of raw janus file (debug purposes)

--- user/hidra/misc/hexdump_fers2025.py
+++ user/hidra/misc/hexdump_fers2025.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 import sys
+import struct
 
-def dump_file(fname):
+
+def dump_file(fname, decode = True):
     with open(fname, "rb") as f:
         data = f.read()
 
     size = len(data)
     i = 0
+
+    def decode_data_header(start):
+        idx, l = (start, 2)
+        eventsize = struct.unpack('<h',data[idx:idx+l])[0]
+        idx, l = (idx +l, 1)
+        board = struct.unpack('<b',data[idx:idx+l])[0]
+        idx, l = (idx +l, 8)
+        timestamp = struct.unpack('<d',data[idx:idx+l])[0]
+        idx, l = (idx +l, 8)
+        trigid = struct.unpack('<q',data[idx:idx+l])[0]
+        idx, l = (idx +l, 8)
+        mask = struct.unpack('<Q',data[idx:idx+l])[0]
+        
+        return f"eh_{start:09d}: size {eventsize} board {board} ts {timestamp:.3f} tg {trigid} chan {bin(mask).count('1')}"
+        
+        
 
     def print_bytes(start, count, newline_every=None):
         idx = start
@@ -43,8 +61,11 @@ def dump_file(fname):
         N = int.from_bytes(data[i:i+2], byteorder="little")
 
         # First 27 bytes (including those 2)
+        iheader = i
         i = print_bytes(i, 27)
         print("\n")
+        if decode:
+            print(decode_data_header(iheader))
 
         # Remaining bytes
         remaining = max(0, N - 27)
