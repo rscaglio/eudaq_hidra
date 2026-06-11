a8ac2a64ac740667bffce831836ce3802d997a14
Ruggero Turra <ruggero.turra@cern.ch>
2026-06-11 23:04:10 +0200

    update tracker

--- user/hidra/misc/hidra_backup_data.sh
+++ user/hidra/misc/hidra_backup_data.sh
@@ -16,6 +16,9 @@ LOCAL_DIR_DATA="${EUDAQHIDRA}/run/out_data"
 BACKUP_DIR_DATA="/home/eudaq/cernbox/TB2026_H8/raw"
 LOCAL_DIR_LOG="${EUDAQHIDRA}/run/logs"
 BACKUP_DIR_LOG="/home/eudaq/cernbox/TB2026_H8/logs"
+LOCAL_DIR_TRACKER_DATA="/home/eudaq/TB2026_TrackerData"
+BACKUP_DIR_TRACKER_DATA="/home/eudaq/cernbox/TB2026_H8/tracker_data"
+BACKUP_DATE_FILE="/home/eudaq/TB2026_H8_last_backup_dates.log"
 
 ##############################################################################
 # Transfer
@@ -47,6 +50,17 @@ rsync \
     --stats \
     "${LOCAL_DIR_LOG}/" \
     "${BACKUP_DIR_LOG}/"
+
+rsync \
+    -av \
+    --update \
+    --partial \
+    --human-readable \
+    --stats \
+    "${LOCAL_DIR_TRACKER_DATA}/" \
+    "${BACKUP_DIR_TRACKER_DATA}/"
+
+date '+%Y-%m-%d %H:%M:%S %Z' >> "${BACKUP_DATE_FILE}"
 
 echo "============================================================"
 echo "Done."
